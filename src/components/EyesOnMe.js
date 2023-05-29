e5a09881519eeeb9dce8e549bbc73ce1dc4e4d0c
Zhongming <atenliao@gmail.com>
2023-05-29 00:22:50 -0700

    update components

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+   
+    return (
+        <div>
+            <button id='btn' onFocus={(e)=>{console.log('Good!')}} onBlur={(e)=>{console.log('Hey! Eyes on me!')}}>
+                Eyes on me
+            </button>
+        {/* <script>
+            {
+            document.querySelector('button').addEventListener('focus',()=>{console.log('Good')})}
+            {
+            document.querySelector('button').addEventListener('blur',() => {console.log('Hey! Eyes on me')})
+            }
+
+        </script> */}
+        </div>
+     
+    )
+}
+
+export default EyesOnMe
